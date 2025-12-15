83e6da6194a8bdd5196c0d9e148da6ca5aed3f00
Sakshi more <moresakshi796@gmail.com>
2025-12-15 17:55:26 +0530

    Add files via upload

---
+++ Oops.java
@@ -0,0 +1,21 @@
+
+class Oops{
+    public static void main(String[]args){
+     int day = 5;
+     switch(day){
+      case 1 :
+        System.out.println("monday");
+        break;
+        case 2:
+          System.out.println("tuesday");
+          break;
+          case 3:
+            System.out.println("wednasday");
+            break;
+            default:
+              System.out.println("Invalid number");
+         }
+  }
+      
+      
+}
